6be21461743921231c57f162b9c71a1bf3a0fd61
Hayman04 <78484204+Hayman04@users.noreply.github.com>
2024-12-15 22:14:23 +0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-# simple-interest.sh
-# Usage: ./simple-interest.sh <principal> <rate> <time>
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <principal> <rate> <time>"
-    exit 1
-fi
+# Simple Interest Calculation
 
 principal=$1
 rate=$2
 time=$3
 
-simple_interest=$(echo "$principal * $rate * $time / 100" | bc -l)
-echo "Simple Interest: $simple_interest"
+simple_interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
+echo "The simple interest is: $simple_interest"
